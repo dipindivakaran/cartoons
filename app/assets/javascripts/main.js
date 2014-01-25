03615450a1f82c7ad0e8d6128a5b2f07457fd694
Dipin <dipin@stayntouch.com>
2014-01-26 00:40:55 +0530

    Added canvas for image draw

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,38 +1,70 @@
-$(document).ready(function(){
-	
-	
+$(document).ready(function() {
+
 	$.ajax({
 		type : "GET",
 		url : "/cartoons",
-		dataType:'json',
-		success : function(msg){
+		dataType : 'json',
+		success : function(msg) {
 			$.each(msg, function(key, value) {
-			$( "#cartoons" ).append("<span class='cartoon' id='image"+key+"'><img class='cartoon-image' src='"+value+"'></img></span>");
-		}); 
-		init_drag_and_drop();	
-		}
-				
-	}); 
-		
-	});
-	
- function init_drag_and_drop(){
- 	$( "#catalog" ).accordion();
-		$( ".cartoon" ).draggable({
-			helper: "clone",
-			revert: "invalid"
+				$("#cartoons").append("<span class='cartoon' id='image" + key + "'><img class='cartoon-image' src='" + value + "'></img></span>");
 			});
-		$( "#cart" ).droppable({
-			activeClass: "ui-state-default",
-			hoverClass: "ui-state-hover",
-			accept: ":not(.ui-sortable-helper)",
-			drop: function( event, ui ) {
-				console.log("dropped");
-				$( this ).find( ".placeholder" ).remove();
-				image_id = ui.draggable.attr('id');
-				html_element = $("#"+image_id).html();
-				$( "<div class='workspace-cartoon'></div>" ).html(html_element).appendTo( this );
-				$(".workspace-cartoon").draggable();
-			}
+			init_drag_and_drop();
+		}
+	});
+
+	$("#convert").click(function() {
+		var oCanvas = document.getElementById("#myCanvas");  
+		
+		var strDataURI = oCanvas.toDataURL("image/jpeg");  
+		alert(strDataURI);
+
+		var data = "<svg xmlns='http://www.w3.org/2000/svg' width='200' height='200'>" + "<foreignObject width='100%' height='100%'>" + "<div xmlns='http://www.w3.org/1999/xhtml'>" +
+
+		/// extract the html content of div
+		document.getElementById('cart').innerHTML + "</div>" + "</foreignObject>" + "</svg>";
+
+		var DOMURL = self.URL || self.webkitURL || self;
+		var img = new Image();
+		var svg = new Blob([data], {
+			type : "image/svg+xml;charset=utf-8"
 		});
- }
+		var reader = new window.FileReader();
+		reader.readAsDataURL(svg);
+		reader.onloadend = function() {
+			base64data = reader.result;
+			alert(base64data);
+			$.post('upload',{image : base64data});
+       };
+		/// create an url that we can use for the image tag
+		var url = DOMURL.createObjectURL(svg);
+		img.onload = function() {
+
+			/// now we can draw the "html" to canvas.
+			ctx.drawImage(img, 0, 0);
+			DOMURL.revokeObjectURL(url);
+		};
+		img.src = url;
+
+	});
+
+});
+
+function init_drag_and_drop() {
+	$("#catalog").accordion();
+	$(".cartoon").draggable({
+		helper : "clone",
+		revert : "invalid"
+	});
+	$("#workspace").droppable({
+		activeClass : "ui-state-default",
+		hoverClass : "ui-state-hover",
+		accept : ":not(.ui-sortable-helper)",
+		drop : function(event, ui) {
+			
+			 var context = $(this)[0].getContext("2d");
+			 
+			context.fillHtml($(ui.draggable).clone());
+			$("#workspace > img").draggable();
+		}
+	});
+}
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
-  
+  skip_before_filter :verify_authenticity_token  
   def index
    
   end
@@ -11,6 +11,14 @@ class DashboardController < ApplicationController
       files << "assets/cartoons/#{file}"
     end
    render json: files
+  end
+  
+   def upload
+    data_url = params[:image]
+    png      = Base64.decode64(params[:image])
+File.open(File.join("public", 'file.png'), 'w') { |f| f.write(png) }
+    
+    
   end
   
   
