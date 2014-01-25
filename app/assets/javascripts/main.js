1bc2fc5346b8db41be6538105ef69c249a858227
Dipin <dipin@stayntouch.com>
2014-01-25 21:38:00 +0530

    Added drop functionality

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,36 +4,35 @@ $(document).ready(function(){
 	$.ajax({
 		type : "GET",
 		url : "/cartoons",
-		dataType:'json'
-			}).done(function(msg) {
-		$.each(msg, function(key, value) {
-			$( "#cartoons" ).append('<li><img src="'+value+'"></img></li>');
+		dataType:'json',
+		success : function(msg){
+			$.each(msg, function(key, value) {
+			$( "#cartoons" ).append("<span class='cartoon' id='image"+key+"'><img class='cartoon-image' src='"+value+"'></img></span>");
 		}); 
+		init_drag_and_drop();	
+		}
 				
 	}); 
-
+		
+	});
 	
-	
-	
-		$( "#catalog" ).accordion();
-		$( "#cartoons li" ).draggable({
-			appendTo: "body",
-			helper: "clone"
-		});
-		$( "#cart ol" ).droppable({
+ function init_drag_and_drop(){
+ 	$( "#catalog" ).accordion();
+		$( ".cartoon" ).draggable({
+			helper: "clone",
+			revert: "invalid"
+			});
+		$( "#cart" ).droppable({
 			activeClass: "ui-state-default",
 			hoverClass: "ui-state-hover",
 			accept: ":not(.ui-sortable-helper)",
 			drop: function( event, ui ) {
+				console.log("dropped");
 				$( this ).find( ".placeholder" ).remove();
-				$( "<li></li>" ).text( ui.draggable.text() ).appendTo( this );
-			}
-		}).sortable({
-			items: "li:not(.placeholder)",
-			sort: function() {
-				// gets added unintentionally by droppable interacting with sortable
-				// using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
-				$( this ).removeClass( "ui-state-default" );
+				image_id = ui.draggable.attr('id');
+				html_element = $("#"+image_id).html();
+				$( "<div class='workspace-cartoon'></div>" ).html(html_element).appendTo( this );
+				$(".workspace-cartoon").draggable();
 			}
 		});
-	});
+ }
